507405126d87fefa0c031f60be49ed0cd5951a99
damiancipolat <damian.cipolat@gmail.com>
2021-05-02 05:34:12 -0300

    subo upgrades

---
+++ BE/Comision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Comision
+    {
+        public int idcomision;
+        public string operacion;
+        public string referencia;
+        public Moneda moneda;
+        public float Valor;
+        public DateTime fecCobro;
+    }
+}
