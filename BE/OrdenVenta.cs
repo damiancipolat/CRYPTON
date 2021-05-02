64854db7b4162d10e625aa4212642a46851844d2
damiancipolat <damian.cipolat@gmail.com>
2021-05-02 05:28:34 -0300

    subo

---
+++ BE/OrdenVenta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public enum OrdenEstado
+    {
+        DISPONIBLE = 1,
+        VENDIDA = 2,
+        EXPIRADA = 3
+    }
+
+    public class OrdenVenta
+    {
+        public int idVenta;
+        public Usuario vendedor;
+        public int cantidad;
+        public Moneda moneda;
+        public float precio;
+        public OrdenEstado estado;
+    }
+
+    public class OrdenCompra
+    {
+        public int idCompra;
+        public OrdenVenta venta;
+        public DateTime fecOperacion;
+        public Cliente comprador;
+        public Moneda moneda;
+        public int cantidad;
+        public float precio;
+    }
+}
---
+++ BE/Transferencia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Transferencia
+    {
+        public int idTransferencia;
+        public DateTime fecProc;
+        public Usuario usuario;
+        public Billetera origen;
+        public Billetera destino;
+        public float valor;
+        public Moneda moneda;
+    }
+}
