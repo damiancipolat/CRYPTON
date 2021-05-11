cfd10f153d975c69bd7a856a640678180534d081
damiancipolat <damian.cipolat@gmail.com>
2021-05-11 03:12:45 -0300

    a

--- DAL/EmpleadoDAL.cs
+++ DAL/EmpleadoDAL.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
+using BE;
 
 namespace DAL
 {
-    public class EmpleadoDAL
+    public class EmpleadoDAL:AbstractDAL<EmpleadoBE>
     {
     }
 }
