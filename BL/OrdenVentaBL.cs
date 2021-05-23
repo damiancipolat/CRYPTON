7837a12af6a28c796c3c4d84cfbc4a145ed7b4bc
damiancipolat <damian.cipolat@gmail.com>
2021-05-23 02:36:28 -0300

    add

---
+++ BL/OrdenVentaBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DAL;
+
+namespace BL
+{
+    public class OrdenVentaBL
+    {
+        public OrdenVentaBE create(OrdenVentaBE venta)
+        {
+            int newId = new OrdenVentaDAL().save(venta);
+            venta.idorden = newId;
+
+            return venta;
+        }
+    }
+}
